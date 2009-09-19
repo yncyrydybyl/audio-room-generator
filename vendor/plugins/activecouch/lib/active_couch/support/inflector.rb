0e101ddf03f91d49fb5854cbef62fbebfbc71438
Anselm Helbig <anselm.helbig@gmx.de>
2009-09-20 00:38:18 +0200

    ruby1.9 compatibility fix

--- vendor/plugins/activecouch/lib/active_couch/support/inflector.rb
+++ vendor/plugins/activecouch/lib/active_couch/support/inflector.rb
@@ -266,9 +266,9 @@ module ActiveCouch
         "#{number}th"
       else
         case number.to_i % 10
-          when 1: "#{number}st"
-          when 2: "#{number}nd"
-          when 3: "#{number}rd"
+          when 1; "#{number}st"
+          when 2; "#{number}nd"
+          when 3; "#{number}rd"
           else    "#{number}th"
         end
       end
