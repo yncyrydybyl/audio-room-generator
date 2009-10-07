19007e7c405b4631ddaaab63f48f1ea297686f7d
Anselm Helbig <anselm.helbig@gmx.de>
2009-10-08 00:24:36 +0200

    added loop for templating; removed non-working test for remote file

--- couchapp/_attachments/script/arg.js
+++ couchapp/_attachments/script/arg.js
@@ -1,21 +1,18 @@
 (function() {
- 
-
   $(document).ready(function() {
     var tmpl = {};
-    $.get("js_templates/post.html", function(data){
-      tmpl.post = data;
+    $.each(['input_text'], function(i, v) {
+      $.get("js_templates/" + v + ".html", function(data){
+        tmpl[v] = data;
+      });
     });
     $("#new_room").bind("click", function(e) {
         $("#form_holder").hide();
         $("#form_holder").animate({backgroundColor: '#fff', color: '#000', width: 240}, 1000);
 
-        $("#form_holder").html(template(tmpl.post, { label: "name", value: "" }) + "<br />" +
-                               template(tmpl.post, { label: "foo", value: "" }));
+        $("#form_holder").html(template(tmpl.input_text, { label: "name", value: "" }) + "<br />" +
+                               template(tmpl.input_text, { label: "foo", value: "" }));
         $("#form_holder input").bind("change", function() {
-            $.head(this.value, {}, function(headers) {
-                console.log(headers); });
-            // console.log("%o: %o", this.name, this.value);
         });
         $("#new_room").hide();
     });
