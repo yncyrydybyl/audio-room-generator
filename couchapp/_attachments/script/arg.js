3ac5ca882cdd98764a223a0ec1a3c5bfd766f3c4
Yan Minagawa <t@time.(none)>
2009-10-08 00:16:53 +0200

    created js_templates

--- couchapp/_attachments/script/arg.js
+++ couchapp/_attachments/script/arg.js
@@ -1,20 +1,23 @@
 (function() {
+ 
 
-		alert("doii");
-    var tmpl = "<label><%= label %> <input type=\"text\" name=\"<%= label %>\" value=\"<%= value %>\"></label>";
-    $(document).ready(function() {
-        $("#new_room").bind("click", function(e) {
-            $("#form_holder").hide();
-						$("#form_holder").animate({backgroundColor: '#fff', color: '#000', width: 240}, 1000);
-
-            $("#form_holder").html(template(tmpl, { label: "name", value: "" }) + "<br />" +
-                                   template(tmpl, { label: "foo", value: "" }));
-            $("#form_holder input").bind("change", function() {
-                $.head(this.value, {}, function(headers) {
-                    console.log(headers); });
-                // console.log("%o: %o", this.name, this.value);
-            });
-						$("#new_room").hide();
-        });
+  $(document).ready(function() {
+    var tmpl = {};
+    $.get("js_templates/post.html", function(data){
+      tmpl.post = data;
     });
+    $("#new_room").bind("click", function(e) {
+        $("#form_holder").hide();
+        $("#form_holder").animate({backgroundColor: '#fff', color: '#000', width: 240}, 1000);
+
+        $("#form_holder").html(template(tmpl.post, { label: "name", value: "" }) + "<br />" +
+                               template(tmpl.post, { label: "foo", value: "" }));
+        $("#form_holder input").bind("change", function() {
+            $.head(this.value, {}, function(headers) {
+                console.log(headers); });
+            // console.log("%o: %o", this.name, this.value);
+        });
+        $("#new_room").hide();
+    });
+  });
 })();
