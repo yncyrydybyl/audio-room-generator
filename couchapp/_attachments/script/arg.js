dba0ea172dfa2cdb671f4da3ec1e7628088ba6b0
Yan Minagawa <t@time.(none)>
2009-10-07 23:12:02 +0200

    playing around with jquery

--- couchapp/_attachments/script/arg.js
+++ couchapp/_attachments/script/arg.js
@@ -1,14 +1,20 @@
 (function() {
+
+		alert("doii");
     var tmpl = "<label><%= label %> <input type=\"text\" name=\"<%= label %>\" value=\"<%= value %>\"></label>";
     $(document).ready(function() {
         $("#new_room").bind("click", function(e) {
-            $("#form_holder").html(template(tmpl, { label: "name", value: "" }) +
+            $("#form_holder").hide();
+						$("#form_holder").animate({backgroundColor: '#fff', color: '#000', width: 240}, 1000);
+
+            $("#form_holder").html(template(tmpl, { label: "name", value: "" }) + "<br />" +
                                    template(tmpl, { label: "foo", value: "" }));
             $("#form_holder input").bind("change", function() {
                 $.head(this.value, {}, function(headers) {
                     console.log(headers); });
                 // console.log("%o: %o", this.name, this.value);
             });
+						$("#new_room").hide();
         });
     });
 })();
